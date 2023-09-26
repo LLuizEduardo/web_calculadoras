f86f610dccfba95e5ac72c2f7d0aee4ded4ca48b
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-09-26 20:29:59 -0300

    ajustes form

--- AvistaOuCartao/script.js
+++ AvistaOuCartao/script.js
@@ -1,5 +1,5 @@
-function Calcular() {
-
+function calcular() {
+// debugger
     let valorTotal = GetValueId('valorTotal');
     let parcelas = GetValueId('qtdParcelas');
     let retornoInvestimento = GetValueId('retornoInvestimento');
@@ -12,11 +12,13 @@ function Calcular() {
 
         var retornoTotal = (valorTotal * retornoInvestimento * (parcelas - 1)) / 2;
 
-        document.getElementById('resultadoInvestimento').textContent = 'Retorno do investimento: ' + retornoTotal;
+        // document.getElementById('resultadoInvestimento').textContent = 'Retorno do investimento: ' + retornoTotal;
+        document.getElementById('resultadoInvestimento').innerHTML = 'Retorno do investimento: ' + retornoTotal;
 
         if (desconto != "") {
             desconto = desconto * valorTotal;
-            document.getElementById('resultadoDesconto').textContent = 'Valor do desconto: ' + desconto;
+            // document.getElementById('resultadoDesconto').textContent = 'Valor do desconto: ' + desconto;
+            document.getElementById('resultadoDesconto').innerHTML = 'Valor do desconto: ' + desconto;
         }
     }
     else {
