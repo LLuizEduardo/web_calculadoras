9a27cad60688113d955b767814a6528b7c6d314c
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-09-27 13:22:42 -0300

    ajustes

--- AvistaOuCartao/script.js
+++ AvistaOuCartao/script.js
@@ -1,31 +1,33 @@
 function calcular() {
-// debugger
+    // debugger
     let valorTotal = GetValueId('valorTotal');
     let parcelas = GetValueId('qtdParcelas');
     let retornoInvestimento = GetValueId('retornoInvestimento');
     let desconto = GetValueId('desconto');
 
-    retornoInvestimento = retornoInvestimento/100;
-    desconto = desconto/100;
- 
+    retornoInvestimento = retornoInvestimento / 100;
+    desconto = desconto / 100;
+
     if (parcelas != "" && valorTotal != "" && retornoInvestimento != "") {
 
-        var retornoTotal = (valorTotal * retornoInvestimento * (parcelas - 1)) / 2;
+        let retornoTotal = (valorTotal * retornoInvestimento * (parcelas - 1)) / 2;
 
-        // document.getElementById('resultadoInvestimento').textContent = 'Retorno do investimento: ' + retornoTotal;
-        document.getElementById('resultadoInvestimento').innerHTML = 'Retorno do investimento: ' + retornoTotal;
+        // document.getElementById('resultadoInvestimento').innerHTML = 'Retorno do investimento: ' + retornoTotal;
+        let resultado = 'Retorno do investimento: ' + retornoTotal;
 
         if (desconto != "") {
             desconto = desconto * valorTotal;
-            // document.getElementById('resultadoDesconto').textContent = 'Valor do desconto: ' + desconto;
-            document.getElementById('resultadoDesconto').innerHTML = 'Valor do desconto: ' + desconto;
+            // document.getElementById('resultadoDesconto').innerHTML = 'Valor do desconto: ' + desconto;
+            resultado += '\nValor do desconto: ' + desconto;
         }
+
+        alert(resultado);
     }
     else {
         alert('Preencha todos os campos corretamente!')
     }
 }
 
-function GetValueId(id){
+function GetValueId(id) {
     return document.getElementById(id).value;
 }
