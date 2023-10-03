ae0d9fe1ee476a57c4b0edbc8dc13a7dbb9c4db5
Luiz Eduardo Fernandes Junior <luiz.eduardo@aec.com.br>
2023-10-03 11:17:38 -0300

    arquivos unicos .js e .css

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
-debugger
+///////////////////////////////////////////////////////////////////////////////
+//Geral
+function GetValueId(id) {
+    return document.getElementById(id).value;
+}
+
+
 fetch("/irpf.json").then((response) => {
     response.json().then((aliquotas)=>{
         console.log(aliquotas);
     })
 })
+
+
+///////////////////////////////////////////////////////////////////////////////
+//A vista ou cartao
+function calcular() {
+    let valorTotal = GetValueId('valorTotal');
+    let parcelas = GetValueId('qtdParcelas');
+    let retornoInvestimento = GetValueId('retornoInvestimento');
+    let desconto = GetValueId('desconto');
+
+    retornoInvestimento = retornoInvestimento / 100;
+    desconto = desconto / 100;
+
+    if (parcelas != "" && valorTotal != "" && retornoInvestimento != "") {
+
+        let retornoTotal = (valorTotal * retornoInvestimento * (parcelas - 1)) / 2;
+
+        // document.getElementById('resultadoInvestimento').innerHTML = 'Retorno do investimento: ' + retornoTotal;
+        let resultado = 'Retorno do investimento: ' + retornoTotal;
+
+        if (desconto != "") {
+            desconto = desconto * valorTotal;
+            resultado += '\nValor do desconto: ' + desconto;
+        }
+
+        alert(resultado);
+    }
+    else {
+        alert('Preencha todos os campos corretamente!')
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//Retorno investimentos
+function calcularInvestimento() {
+
+    let valorInvestimento = GetValueId('valorInvestimento');
+    let qtdmeses = GetValueId('qtdmeses');
+    let retornoInvestimento = GetValueId('retornoInvestimento');
+
+    retornoInvestimento = retornoInvestimento / 100;
+
+    if (valorInvestimento != "" && qtdmeses != "" && retornoInvestimento != "") {
+
+        let retornoTotal = valorInvestimento * (1 + retornoInvestimento) ** qtdmeses;
+
+        let resultado = 'Retorno do investimento: ' + retornoTotal;
+
+        alert(resultado);
+    }
+    else {
+        alert('Preencha todos os campos corretamente!')
+    }
+}
